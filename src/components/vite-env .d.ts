c0cd5317283995a48de25731d2b711d33df8e9db
MrunmayeeCom <mrunmayeecom@gmail.com>
2026-01-15 12:43:29 +0530

    f

--- src/components/vite-env .d.ts
+++ src/components/vite-env .d.ts
@@ -1,16 +1,6 @@
-declare module '*.png' {
-  const value: string;
-  export default value;
-}
-declare module '*.jpg' {
-  const value: string;
-  export default value;
-}
-declare module '*.jpeg' {
-    const value: string;
-    export default value;
-}
-declare module '*.svg' {
-  const value: string;
-  export default value;
-}
+/// <reference types="vite/client" />
+
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.svg";
